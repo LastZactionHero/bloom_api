3df033bcf0ce5417604d6c485e3740a79b2fdbb4
= <=>
2017-02-07 12:43:21 -0700

    Improve RectPositionPattern; Tests

--- app/lib/plant_grid/position_patterns/rect_position_pattern.rb
+++ app/lib/plant_grid/position_patterns/rect_position_pattern.rb
@@ -1,11 +1,17 @@
 module PlantGrid
   class RectPositionPattern < PositionPattern
     def position
+      # Determine how many plants will fit in this area
       plants_wide = (@width.to_f / (@plant.width_with_horizontal_spacing)).floor
       plants_tall = (@height.to_f / (@plant.width_with_vertical_spacing)).floor
 
-      start_x = ((@width - ((plants_wide - 1) * @plant.horizontal_spacing + plants_wide * @plant.width)) / 2).ceil
-      start_y = ((@height - ((plants_tall - 1) * @plant.vertical_spacing + plants_tall * @plant.width)) / 2).ceil
+      # Determine the size of the box containing all plants
+      plants_rect_width = @plant.width_with_horizontal_spacing * plants_wide
+      plants_rect_height = @plant.width_with_vertical_spacing * plants_tall
+
+      # Calculate the offset to start placing plants, centered in the bed
+      start_x = ((@width - plants_rect_width) / 2 + @plant.width_with_horizontal_spacing / 2).ceil
+      start_y = ((@height - plants_rect_height) / 2 + @plant.width_with_vertical_spacing / 2).ceil
 
       (0..(plants_wide - 1)).each do |plant_x|
         (0..(plants_tall - 1)).each do |plant_y|
