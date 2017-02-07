8427fd854b39e607421d5ae3e8f439a1b9e337cf
= <=>
2017-02-07 10:47:05 -0700

    Move PlantGrid to Lib

--- app/lib/plant_grid/old_templates.rb
+++ app/lib/plant_grid/old_templates.rb
@@ -1,19 +1,3 @@
-require 'json'
-
-require './position_patterns/position_pattern'
-require './position_patterns/rect_position_pattern'
-
-require './grid_cells/grid_cell'
-require './grid_cells/composite_grid_cell'
-require './grid_cells/plant_grid_cell'
-require './grid_cells/empty_grid_cell'
-
-require './bed_template'
-require './plant_template'
-require './grid'
-require './printer'
-
-# 
 # # Modern/Linear A
 # # cell_a = PlantGridCell.new(1.0, 0.6, Plant.new('A', 3, 2, 2), RectPositionPattern)
 # # cell_b = PlantGridCell.new(1.0, 0.4, Plant.new('B', 2, 1, 1), RectPositionPattern)
@@ -38,7 +22,3 @@ require './printer'
 # # cell_b = PlantGridCell.new(0.4, 1.0, Plant.new('B', 3, 12, 3), RectPositionPattern)
 # # cell_c = PlantGridCell.new(0.3, 1.0, Plant.new('A', 2, 6, 2), RectPositionPattern)
 # # cell_composite = CompositeGridCell.new(1.0, 1.0, :horizontal, [cell_a, cell_b, cell_c])
-# 
-# grid = Grid.new(template.cell, 120, 30)
-# printer = Printer.new(grid)
-# puts printer.draw
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Bloom
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    require "#{Rails.root}/lib/plant_grid/plant_grid"
+    Dir.glob(File.join("#{Rails.root}/app/lib", '**', '*.rb'), &method(:require))
+    # 
+    # require_all "#{Rails.root}/app/lib"
   end
 end
