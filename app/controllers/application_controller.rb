039f4bc2999a4cf9624e472fa83463fcb140fb25
= <=>
2017-02-06 22:27:03 -0700

    CORS update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   def cors
     response.headers['Access-Control-Allow-Origin'] = request.origin
     response.headers['Access-Control-Allow-Headers'] = request.headers['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']
+    response.headers['Access-Control-Allow-Methods'] = "GET,POST,PATCH,DELETE,OPTIONS,PUT";
   end
 
   private
@@ -13,5 +14,6 @@ class ApplicationController < ActionController::Base
   def cors_headers
     response.headers['Access-Control-Allow-Origin'] = request.origin
     response.headers['Access-Control-Allow-Headers'] = request.headers['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']
+    response.headers['Access-Control-Allow-Methods'] = "GET,POST,PATCH,DELETE,OPTIONS,PUT";
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match '/search/query', to: 'application#cors', via: [:options]
   match '/search/options', to: 'application#cors', via: [:options]
+  match '/bed_templates', to: 'application#cors', via: [:options]
+  match '/bed_templates/:id/placements', to: 'application#cors', via: [:options]
+  match '/bed_templates/:id', to: 'application#cors', via: [:options]
 
   post '/search/query', to: 'search#query'
   get '/search/options', to: 'search#options'
